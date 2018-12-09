33cd40a28afa68129b01f2d45a225683f46d48cf
nimoken <nimomaina.nm@gmail.com>
2018-12-09 22:34:50 +0300

    Adding questions in index page

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$('#showanswer').click(function(){
+     $('.Question, p').show();
+     $('#showanswer').hide();
+
+     $('#hideanswer').show();
+  });
+
+  $('#hideanswer').click(function(){
+
+     $('.answer',).hide();
+     $('#showanswer').show();
+     $('#hideanswer').hide();
+  });
+  });
