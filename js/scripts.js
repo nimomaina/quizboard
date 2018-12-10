8c9e167077d5d766df82868527cc010bfcb0ef14
nimoken <nimomaina.nm@gmail.com>
2018-12-10 17:46:36 +0300

    Correcting functions

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
 
 
   $("form.questions").submit(function(event) {
+    var quiz1 = $("input:radio[name=one]:checked").val();
     var quiz2 = $("input:radio[name=two]:checked").val();
     var quiz3 = $("input:radio[name=three]:checked").val();
     var quiz4 = $("input:radio[name=four]:checked").val();
@@ -55,33 +56,5 @@ $(document).ready(function() {
   });
 
 
-  var visibleDiv = 0;
-  function showDiv(){
-    $('.panel-info:eq('+ visibleDiv +')').show();
-  }
-  showDiv();
 
-  function showNext(){
-    $('.panel-info').hide();
-    if(visibleDiv == $('.panel-info').length){
-      visibleDiv = 0;
-    }else {
-      visibleDiv ++;
-    }
-    showDiv();
-  }
-
-  $('#showanswer').click(function() {
-    $('.Question, p').show();
-    $('#showanswer').hide();
-
-    $('#hideanswer').show();
-  });
-
-  $('#hideanswer').click(function() {
-
-    $('.answer', ).hide();
-    $('#showanswer').show();
-    $('#hideanswer').hide();
-  });
 });
